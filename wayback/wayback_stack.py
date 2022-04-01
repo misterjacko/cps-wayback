d116ba5b23e232a3bbcebc1d0e933447d4a91fdc
Jakob Ondrey <jakob.ondrey@yello.co>
2022-03-31 20:29:01 -0500

    adds event to trigger function

--- wayback/wayback_stack.py
+++ wayback/wayback_stack.py
@@ -1,19 +1,33 @@
 from aws_cdk import (
-    # Duration,
+    aws_lambda as lambda_,
+    aws_events as event,
+    aws_events_targets as targets,
+
     Stack,
-    # aws_sqs as sqs,
 )
 from constructs import Construct
+import aws_cdk.aws_lambda_python_alpha as lambda_python
 
 class WaybackStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "WaybackQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        wayback_function = lambda_python.PythonFunction(
+            self,
+            "wayback",
+            function_name="WaybackArchiver",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            entry="./app",
+            index="app.py",
+            handler="lambda_handler",
+            memory_size=128,
+        )
+
+        event.Rule(self, "WaybackRule",
+            rule_name="WaybackRule",
+            schedule=event.Schedule.cron(minute="0", hour="10"),
+            targets=[targets.LambdaFunction(wayback_function)]
+        )
