8aac1884898dd7bfc3158d46676b1ea980948eee
Jakob <jakob.ondrey@yello.co>
2023-01-26 17:19:14 -0600

    example with no diff

--- pipe.sh
+++ pipe.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo Installing TypeScript
+echo Installing TypeScript...
 npm install typescript
-echo Installing CDK
+
+echo Silently installing CDK...
 npm install -g aws-cdk@2.18.0 >/dev/null 2>&1
-echo Installing Requirements
+cdk --version
+
+echo Silently installing Requirements...
 pip install -r requirements.txt >/dev/null 2>&1
 
-echo CDK Diff
+echo Running CDK Diff
 cdk diff
